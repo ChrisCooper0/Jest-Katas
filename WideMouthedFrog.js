aca9d047d4fdeaa0cceffb5b2d6f7dff4e708319
ChrisCooper0 <christopher.cooper@mail.com>
2021-07-11 10:48:22 +0100

    added kata

--- WideMouthedFrog.js
+++ WideMouthedFrog.js
@@ -1,12 +1,3 @@
-// function mouthSize(animal) {
-//   if (animal === "alligator") {
-//     return "small";
-//   } else {
-//     return "wide";
-//   }
-// }
-
-// Refactored solution
 const mouthSize = (animal) => {
   return animal === "alligator" ? "small" : "wide";
 };
