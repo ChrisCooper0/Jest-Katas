09250e4984540d2e7e48dad6694342a5459c8bbd
ChrisCooper0 <christopher.cooper@mail.com>
2021-07-11 13:09:42 +0100

    add kata

---
+++ tests/BoolToWord.test.js
@@ -0,0 +1,7 @@
+const { expect } = require("@jest/globals");
+const boolToWord = require("../BoolToWord");
+
+test("converts boolean to Yes if true or No if false", () => {
+  expect(boolToWord(true)).toBe("Yes");
+  expect(boolToWord(false)).toBe("No");
+});
