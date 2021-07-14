a4865626f88eab9810078bc3d47dbcac59329b1f
ChrisCooper0 <christopher.cooper@mail.com>
2021-07-14 08:37:03 +0100

    add kata

---
+++ tests/Remove!.test.js
@@ -0,0 +1,17 @@
+const remove = require("../Remove!");
+
+test("removes last exclamation mark from end of string", () => {
+  expect(remove("Hi!!!")).toBe("Hi!!");
+});
+
+test("removes the only exclamation mark from end of string", () => {
+  expect(remove("Hi!")).toBe("Hi");
+});
+
+test("removes ending exclamation mark from end of string, leaving the leading exclamation", () => {
+  expect(remove("!Hi!")).toBe("!Hi");
+});
+
+test("removes last exclamation mark from end of string", () => {
+  expect(remove("Hi! Hi!")).toBe("Hi! Hi");
+});
