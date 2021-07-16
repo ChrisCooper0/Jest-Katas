2ecbf2f63d0b5e855223e07cfc91c96996ca92e9
ChrisCooper0 <christopher.cooper@mail.com>
2021-07-16 08:06:43 +0100

    add kata

---
+++ tests/OppositesAttract.test.js
@@ -0,0 +1,9 @@
+const loveFunc = require("../OppositesAttract")
+
+test(('returns true if one flower is an odd number and the other is even, otherwise returns false'), () => {
+    expect(loveFunc(2,7)).toBe((true));
+})
+
+test(('returns true if one flower is an odd number and the other is even, otherwise returns false'), () => {
+    expect(loveFunc(5,9)).toBe((false));
+})
