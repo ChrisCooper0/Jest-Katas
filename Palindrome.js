c16ac9e060e3c75d350c3fd4d9571bcac7bf2dd5
ChrisCooper0 <christopher.cooper@mail.com>
2021-08-08 14:12:03 +0100

    add kata

---
+++ Palindrome.js
@@ -0,0 +1,10 @@
+const isPalindrome = (str) => {
+  const lowerStr = str.toLowerCase();
+  const revStr = lowerStr.split("").reverse().join("");
+  return lowerStr === revStr ? true : false;
+};
+
+console.log(isPalindrome("Racecar")); // true
+console.log(isPalindrome("abc")); // false
+
+module.exports = isPalindrome;
---
+++ tests/Palindrome.test.js
@@ -0,0 +1,5 @@
+const isPalindrome = require("../Palindrome");
+
+test("Write function that checks if a given string is a palindrome", () => {
+  expect(isPalindrome("Racecar")).toBe(true);
+});
