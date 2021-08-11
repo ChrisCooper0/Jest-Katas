2e559bc847f420f77e550822df1a944c26d0bc8c
ChrisCooper0 <christopher.cooper@mail.com>
2021-08-11 07:59:13 +0100

    add kata

---
+++ ArrayOfSheep.js
@@ -0,0 +1,49 @@
+// Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+
+// For example;
+const arrayOfSheep = [
+  true,
+  true,
+  true,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  true,
+  true,
+];
+
+// // Best practice
+// function countSheeps(arrayOfSheep) {
+//   return arrayOfSheep.filter(Boolean).length;
+// }
+
+// For loop example
+function countSheeps(arrayOfSheep) {
+  let count = 0;
+  for (let i = 0; i < arrayOfSheep.length; i++) {
+    if (arrayOfSheep[i] === true) {
+      count++;
+    }
+  }
+  return count;
+}
+
+// console.log(countSheeps(arrayOfSheep)); // 17
+
+module.exports = countSheeps;
---
+++ tests/ArrayOfSheeps.test.js
@@ -0,0 +1,32 @@
+const countSheeps = require("../ArrayOfSheep");
+
+const arrayOfSheep = [
+  true,
+  true,
+  true,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  true,
+  false,
+  true,
+  false,
+  false,
+  true,
+  true,
+  true,
+  true,
+  true,
+  false,
+  false,
+  true,
+  true,
+];
+
+test("returns number of true values in array", () => {
+  expect(countSheeps(arrayOfSheep)).toBe(17);
+});
