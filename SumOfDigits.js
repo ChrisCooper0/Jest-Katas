38c12685e340d75989f21d7237c068d8af0ebb1f
ChrisCooper0 <christopher.cooper@mail.com>
2021-08-13 22:25:06 +0100

    add instructions

--- SumOfDigits.js
+++ SumOfDigits.js
@@ -1,3 +1,8 @@
+// It involves implementing a program that sums the digits of a non-negative integer. For example, the sum of 3433 digits is 13.
+// Digits can be a number or a string, and you should control it is no undefined and return an empty string.
+// Digits can be a number or a string, and you should ensure it is not None and return an empty string.
+// To give you a little more excitement, the program will not only write the result of the sum, but also write all the sums used: 3 + 4 + 3 + 3 = 13.
+
 function sum(digits) {
   if (typeof digits == "number") {
     digits = JSON.stringify(digits);
@@ -31,4 +36,4 @@ function sum(digits) {
 console.log(sum("12345")); // 1 + 2 + 3 + 4 + 5 = 15
 console.log(sum(1)); // 1 = 1
 console.log(sum(12345)); // 1 + 2 + 3 + 4 + 5 = 15
-console.log(sum()); // 
+console.log(sum()); //
